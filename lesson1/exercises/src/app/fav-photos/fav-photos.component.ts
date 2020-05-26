d5dd4ad2c4ed9f47cb1c5fda0f91a37972e8f910
Dominique <dlsykes91@gmail.com>
2020-05-26 12:16:01 -0400

    updated part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Favorite Images';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fartimaii%2Fart%2FChibi-Kurama-703234464&psig=AOvVaw3irwyURQCzhwCzuiW2AV9b&ust=1590540329655000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiQ9--m0OkCFQAAAAAdAAAAABAD';
+  image3 = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FDans-Decals-Akatsukis-Sticker-Uzamaki%2Fdp%2FB07D5JXSJQ&psig=AOvVaw3YJIV73ERE33VuWgIMO40Y&ust=1590586784234000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCHgvfT0ekCFQAAAAAdAAAAABAI';
 
   constructor() { }
 
